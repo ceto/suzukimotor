96b7805f1251d6fb5ee08f621e24bc30bc0bb02e
Gabor Szabo <szabogabi@gmail.com>
2016-01-07 21:36:56 +0100

    product page

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,23 @@
 // Place any jQuery/helper plugins in here.
 
 jQuery(document).foundation();
+
+$(document).ready(function() {
+
+  $('.owl-prodgal').owlCarousel({
+
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+
+      // "singleItem:true" is a shortcut for:
+      //items : 1
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+});
